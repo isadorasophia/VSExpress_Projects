e1606b2efddaee398fc6c2c0830aee4748453469
isadorasophia <isaecia@gmail.com>
2014-12-01 02:14:19 -0200

    Exercise 2 added!

---
+++ GX102_05/Times/Times/Times/Time/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Times.Time
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public int Hability { get; private set; }
+
+        public Player (string nameInput, int habilityInput)
+        {
+            this.Name = nameInput;
+            this.Hability = habilityInput;
+        }
+    }
+}
