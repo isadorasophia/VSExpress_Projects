c6f238e1c8a252c5297f4dd0c3dd8493bb87df34
isadorasophia <isaecia@gmail.com>
2014-12-01 02:23:43 -0200

    Fixed the location... blerg

---
+++ GX102_05/Times/Times/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Times.Time;
+
+namespace Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineInput = new String[2];
+            int i, nPlayers, nTeams;
+            List<Team> Teams = new List<Team> ();
+            List<Player> Players = new List<Player>();
+
+            // Read input - nah, ain't nobody got time for exception handling for now
+            lineInput = Console.ReadLine().Split(' ');
+            nPlayers = Int32.Parse(lineInput[0]);
+            nTeams = Int32.Parse(lineInput[1]);
+
+            // Create the teams instances
+            for (i = 0; i < nTeams; i++)
+                Teams.Add(new Team());
+
+            // And now add the players
+            while (nPlayers-- > 0)
+            {
+                lineInput = Console.ReadLine().Split(' ');
+                Players.Add(new Player(lineInput[0], Int32.Parse(lineInput[1])));
+            }
+
+            // While there are still players to be placed in teams, add them
+            while (Players.Count != 0)
+            {
+                for (i = 0; i < nTeams && Players.Count != 0; i++)
+                    Teams[i].GetBestPlayer(Players);
+            }
+
+            // Show the players assigned in each team
+            for (i = 0; i < nTeams && Players != null; i++)
+                Console.WriteLine("Time " + (i + 1) + " \r\n" + Teams[i].ShowPlayers() + "\r\n");
+
+            // Exit
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GX102_05/Times/Times/Time/Team.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Times.Time
+{
+    public class Team
+    {
+        public List<Player> Players { get; set; }
+
+        public Team ()
+        {
+            this.Players = new List<Player>();
+        }
+
+        // Pick up the player with the highest hability available for the team
+        public void GetBestPlayer(List<Player> playersAvailable)
+        {
+            int bestID = 0;
+
+            for (int i = 1; i < playersAvailable.Count; i++)
+                if (playersAvailable[i].Hability > playersAvailable[bestID].Hability)
+                    bestID = i;
+
+            this.Players.Add(playersAvailable[bestID]);
+            playersAvailable.RemoveAt(bestID);
+        }
+
+        // Show the players setted in that team
+        public string ShowPlayers()
+        {
+            string playersName = null;
+
+            foreach (Player player in this.Players)
+            {
+                playersName += player.Name + "\r\n";
+            }
+
+            return playersName;
+        }
+    }
+}
